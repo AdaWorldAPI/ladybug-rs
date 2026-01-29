66c0e0270751b14580da0f4822f1f96d367c7e04
AdaWorldAPI <jan@exo.red>
2026-01-29 16:01:24 +0100

    Add src/fabric/mod.rs

---
+++ src/fabric/mod.rs
@@ -0,0 +1,11 @@
+//! Cognitive Fabric - mRNA cross-pollination and butterfly detection
+//!
+//! This is the unified substrate where all subsystems resonate.
+
+pub mod mrna;
+pub mod butterfly;
+pub mod subsystem;
+
+pub use mrna::{MRNA, ResonanceField, CrossPollination};
+pub use butterfly::{ButterflyDetector, Butterfly, ButterflyPrediction};
+pub use subsystem::Subsystem;
